963bbcc6bf35228eaac6c2507eedda58be0980f2
Nageh <moh.nage7@gmail.com>
2022-11-27 17:20:42 +0200

    feat: display shimmering and its preview

--- app/src/main/java/com/mohnage7/tendingrepos/HomeActivity.kt
+++ app/src/main/java/com/mohnage7/tendingrepos/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.mohnage7.tendingrepos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,9 +12,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush.Companion.linearGradient
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mohnage7.tendingrepos.ui.LoadingShimmerEffect
+import com.mohnage7.tendingrepos.ui.ShimmerItem
 import com.mohnage7.tendingrepos.ui.theme.RepoItem
 import com.mohnage7.tendingrepos.ui.theme.TrendingReposTheme
 
@@ -38,6 +42,8 @@ class HomeActivity : ComponentActivity() {
                             )
                         )
                     )
+
+                    ShowShimmering()
                 }
             }
         }
@@ -52,6 +58,30 @@ fun RenderTrendingRepos(reposList: List<TrendingRepo>) {
             Divider(color = Color.LightGray, thickness = 0.5.dp)
         }
     }
+}
+
+@Composable
+fun ShowShimmering() {
+    Column {
+        repeat(15) {
+            LoadingShimmerEffect()
+        }
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun ShimmerPreview() {
+    ShimmerItem(
+        brush = linearGradient(
+            listOf(
+                Color.LightGray.copy(alpha = 0.9f),
+                Color.LightGray.copy(alpha = 0.4f),
+                Color.LightGray.copy(alpha = 0.9f)
+            )
+        )
+    )
 }
 
 @Preview(showBackground = true)
